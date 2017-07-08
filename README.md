82f06ca96a5c8f66c40111ea87c5fbe9c58ed96c
Kasper de Vlaming <k@sperdevlaming.nl>
2017-07-08 15:02:41 +0200

    Install unzip for Metro theme Auto-login for phpMyAdmin Create certificate for nginx Restart nginx Update README.md

--- README.md
+++ README.md
@@ -3,17 +3,25 @@
 Simple shell script that automatically installs the latest version of PhpMyAdmin
 on a Laravel Homestead box.
 
+Based on the work of  [grrnikos](https://github.com/grrnikos/pma), but with some added features:
+
+* Homestead 5.4 support (creation of certificate)
+* Install the latest version of phpMyAdmin
+* Install the Metro theme (I don't like the default)
+* Auto-login phpMyAdmin (be careful running this on public networks)
+
 ## Usage
 
 1. SSH into your Homestead box `homestead ssh` or `vagrant ssh`
 
-2. `cd` to your code/projects directory (by default `~/Code`)
+2. `$ curl -sS https://raw.githubusercontent.com/kdv88/homestead-pma/master/pma.sh | bash`
 
-3. `$ curl -sS https://raw.githubusercontent.com/grrnikos/pma/master/pma.sh | sh`
+3. Open the `/etc/hosts` file on your main machine and add `127.0.0.1  phpmyadmin.app`
 
-4. Open the `/etc/hosts` file on your main machine and add `127.0.0.1  phpmyadmin.app`
+4. Go to [http://phpmyadmin.app:8000](http://phpmyadmin.app:8000).
 
-5. Go to [http://phpmyadmin.app:8000](http://phpmyadmin.app:8000). Default credentials are username `homestead` and password `secret`
+You can also add the contents of pma.sh to the after.sh of your project so
+you don't have to ssh into your vagrant box.
 
 ## License
 
--- pma.sh
+++ pma.sh
@@ -1,18 +1,43 @@
 #!/bin/bash
 
-echo 'Downloading phpMyAdmin 4.7.1'
-curl -#L https://files.phpmyadmin.net/phpMyAdmin/4.7.1/phpMyAdmin-4.7.1-english.tar.gz -o phpmyadmin.tar.gz
+sudo apt-get -qq update
+sudo apt-get -qq -y install unzip
+
+echo 'Downloading latest phpMyAdmin'
+curl -s -#L https://www.phpmyadmin.net/downloads/phpMyAdmin-latest-english.tar.gz -o phpmyadmin.tar.gz
 
 mkdir phpmyadmin && tar xf phpmyadmin.tar.gz -C phpmyadmin --strip-components 1
 
 rm phpmyadmin.tar.gz
 
-CMD=/vagrant/scripts/serve-laravel.sh
+cat << 'EOL' >> phpmyadmin/config.inc.php
+<?php
+$i = 1;
+$cfg['Servers'][$i]['auth_type'] = 'config';
+$cfg['Servers'][$i]['user'] = 'homestead';
+$cfg['Servers'][$i]['password'] = 'secret';
+
+$cfg['ThemeDefault']= 'metro';
+EOL
+
+echo 'Installing phpMyAdmin theme'
+wget --quiet https://files.phpmyadmin.net/themes/metro/2.5/metro-2.5.zip -O temp.zip; unzip -q temp.zip
+
+mv metro phpmyadmin/themes
+rm temp.zip
+
+
+CERT_CMD=/vagrant/vendor/laravel/homestead/scripts/create-certificate.sh
+SERVE_CMD=/vagrant/vendor/laravel/homestead/scripts/serve-laravel.sh
 if [ ! -f $CMD ]; then
+    echo "Older version detected."
     # fallback for older versions
     CMD=/vagrant/scripts/serve.sh
 fi
 
-sudo bash $CMD phpmyadmin.app $(pwd)/phpmyadmin
+echo "Creating certificate"
+sudo bash $CERT_CMD phpmyadmin.app
+echo "Serving new phpmyadmin.app"
+sudo bash $SERVE_CMD phpmyadmin.app $(pwd)/phpmyadmin
 
-sudo service nginx reload
+sudo systemctl restart nginx.service
